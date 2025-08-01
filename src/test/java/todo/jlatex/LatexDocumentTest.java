5d902daa2a7274fbb0452869e7a809c25441f147
glasergl <gabriel.glaser@web.de>
2025-08-01 06:24:09 +0200

    improve test case

--- src/test/java/todo/jlatex/LatexDocumentTest.java
+++ src/test/java/todo/jlatex/LatexDocumentTest.java
@@ -10,10 +10,10 @@ class LatexDocumentTest {
     @Test
     void test() {
 	final LatexDocument d = new LatexDocument("article");
-	d.usePackage("babel")
+	d.usePackage("babel", "english")
 		.usePackage("amsmath")
 		.beginDocument()
-		.line(LatexCommand.get("frac", "1", "2"))
+		.line(LatexCommand.format("$%$", LatexCommand.get("frac", "1", "2")))
 		.emptyLine()
 		.line("I like Spaghetti with Tomato Sauce.")
 		.line(LatexLine.format("My favourite command is either this $%$ or that $%$", LatexCommand.get("frac", "2\\pi", "3"), LatexCommand.get("textbf", "abcd")))
@@ -22,9 +22,12 @@ class LatexDocumentTest {
 		.endEnvironment("figure")
 		.endDocument();
 	final String latex = d.toString();
-	assertTrue(latex.contains("\\usepackage{babel}"));
+	assertTrue(latex.contains("\\usepackage[english]{babel}"));
 	assertTrue(latex.contains("\\usepackage{amsmath}"));
 	assertTrue(latex.contains("\\frac{1}{2}"));
 	assertTrue(latex.contains("My favourite command is either this $\\frac{2\\pi}{3}$ or that $\\textbf{abcd}$"));
+	assertTrue(latex.contains("\\begin{figure}[h]"));
+	assertTrue(latex.contains("\\includegraphics[width=0.9\\textwidth]{./path/to/figure.png}"));
+	assertTrue(latex.contains("\\end{figure}"));
     }
 }
